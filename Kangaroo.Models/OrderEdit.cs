96f41e2ded6c84808326f441b5c02a2cf6b12bf8
pooja197 <47895436+pooja197@users.noreply.github.com>
2019-05-06 15:40:45 -0400

    foriegnkeys

--- Kangaroo.Models/OrderEdit.cs
+++ Kangaroo.Models/OrderEdit.cs
@@ -12,6 +12,13 @@ namespace Kangaroo.Models
         public string Comments { get; set; }
 
         public override string ToString() => Comments;
-        
+        public int MenuItemId { get; set; }
+        public int CustomerId { get; set; }
+
+        public string MenuItemName { get; set; }
+        public string MenuItemPrice { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+
     }
 }
